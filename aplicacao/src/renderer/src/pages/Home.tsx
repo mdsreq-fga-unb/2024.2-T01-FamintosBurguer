6fd9dafac0156438d66ddb547b3402bfe038705d
isaacbatista26 <isaacbatista2266@gmail.com>
2025-01-25 18:19:27 -0300

    Refatorar componentes para usar tipos TypeScript e melhorar a consistência do código

--- aplicacao/src/renderer/src/pages/Home.tsx
+++ aplicacao/src/renderer/src/pages/Home.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
-const Home = () => (
+const Home: React.FC = () => (
   <div>
     <h1 className="text-3xl font-bold">Home Page</h1>
     <p>Welcome to the home page.</p>
   </div>
-);
+)
 
-export default Home;
+export default Home
