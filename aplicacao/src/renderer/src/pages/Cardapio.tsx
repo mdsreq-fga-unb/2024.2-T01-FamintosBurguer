22047c84fd4f03f6d74e08e3e018338c3b2b4747
Túlio Celeri <celeritulio@terra.com.br>
2025-02-01 16:33:13 -0300

    Ajustando padding do header

--- aplicacao/src/renderer/src/pages/Cardapio.tsx
+++ aplicacao/src/renderer/src/pages/Cardapio.tsx
@@ -140,8 +140,8 @@ const handleFinalizeOrder = (): void => {
     <div className="flex w-full h-full pt-16">
       {/* Área do cardápio */}
       <div className="w-2/3">
-        <div className="fixed top-0 left-64 w-[calc(67%-16rem)] shadow-md flex justify-between items-center px-6 py-4 z-10">
-          <h1 className="text-3xl font-bold text-white">Cardápio</h1>
+        <div className="fixed top-0 left-[100px] w-[calc(70%-16rem)] shadow-md flex justify-between items-center  py-4 z-10">
+          <h1 className="text-3xl font-bold px-6 text-white">Cardápio</h1>
           <button
             onClick={handleAddItem}
             className="flex items-center justify-center gap-2 bg-purple-700 text-white px-4 py-2 rounded-lg shadow hover:bg-purple-800 transition-all"
